5dd6e22b6cca4793aad4583d78d80d8c535cbbbe
thaishoaf <20520510@gm.uit.edu.vn>
2022-11-16 17:53:39 +0700

    login-home-footer-header-signup-register

--- src/models/connection.js
+++ src/models/connection.js
@@ -4,7 +4,7 @@ require('dotenv').config({path: './.env'});
 const URL = process.env.DATABASE_URL;
 async function connect() {
     try {
-        await mongoose.connect(URL,{
+        await mongoose.connect("mongodb+srv://gemstonestore:gemstonestore123456@cluster0.efw7idq.mongodb.net/?retryWrites=true&w=majority",{
             useNewUrlParser: true, 
             useUnifiedTopology: true,
         });
