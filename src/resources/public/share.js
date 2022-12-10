2ec3e29417e9db3999ae670edd4919697643a2d0
PhanNhaan <20520258@gm.uit.edu.vn>
2022-12-10 17:38:12 +0700

    priceformat
    hàm không nhận dataset.value nên trang bị lỗi

--- src/resources/public/share.js
+++ src/resources/public/share.js
@@ -1,6 +1,16 @@
 function ConvertToVND(a){
+    
     for(let i=0;i<=a.length;i++){
-        let value = new Intl.NumberFormat('it-IT', { style: 'currency', currency: 'VND' }).format(a[i].dataset.value);
+        let price_value = a[i].dataset.value;
+        let value = new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(price_value);
         a[i].innerHTML=value;
     }
 }
+
+// let price = document.querySelectorAll('.price_format');
+// for (let i = 0; i < price.length; i++) {
+//     let price_value=price[i].dataset.value;
+//     console.log(price_value);
+//     let value_new = new Intl.NumberFormat('it-IT', { style: 'currency', currency: 'VND' }).format(price_value);
+//     price[i].innerHTML = value_new;
+// }
