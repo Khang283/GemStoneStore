9175a0729ec8a4aa2fc9b76f8f7dd0161f9a1f4c
= <=>
2022-12-11 09:51:03 +0700

    fixed minor bugs

--- src/resources/public/share.js
+++ src/resources/public/share.js
@@ -1,6 +1,8 @@
 function ConvertToVND(a){
     for(let i=0;i<=a.length;i++){
-        let value = new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(a[i].dataset.value);
-        a[i].innerHTML=value;
+        if(a[i].dataset.value!=undefined){
+            let value = new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(a[i].dataset.value);
+            a[i].innerHTML=value;
+        }
     }
 }
